9b2e41c6782ba6f61c1741a78a8c618d62bd82b5
raed charrad <raed.charrad91@gmail.com>
2022-04-11 11:08:58 +0100

    chanson

---
+++ probleme/DS/src/chanson.java
@@ -0,0 +1,57 @@
+public class Chanson {
+	private String titre;
+	private int durée;
+		
+	public Chanson() {
+		this.saisirChanson();
+	}
+	public String getTitre() {
+		return titre;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	public int getDurée() {
+		return durée;
+	}
+	public void setDurée(int durée) {
+		this.durée = durée;
+	}
+	
+	@Override
+	public String toString() {
+	return "Chanson [titre=" + titre + ", durée=" + durée + "]";
+	}
+	public int lireDuree() throws InvalidDureeException{
+		Scanner clavier = new Scanner(System.in);
+		int d = clavier.nextInt();
+		if (d<0){
+			throw new InvalidDureeException("Durée négative");
+		}
+		else return d;
+	}
+	public String lireTitre()throws InvalidTitreException{
+		Scanner clavier = new Scanner(System.in);
+		String t = clavier.nextLine();
+		if (t.length()>20){
+			throw new InvalidTitreException("Titre tros long ");
+		}
+		else return t;
+	}
+	public void saisirChanson(){
+		do{
+			try{
+				System.out.println("Saisir le titre de la chanson");
+				this.titre = this.lireTitre();
+				System.out.println("Saisir la durée de la chanson");
+				this.durée = this.lireDuree();
+			}
+			catch (InvalidTitreException e){
+				System.out.println(e.getMessage());
+			}
+			catch (InvalidDureeException e){
+				System.out.println(e.getMessage());
+			}
+		}while(this.titre.length()<20 || this.durée>0);
+	}
+}
